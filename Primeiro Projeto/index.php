28125be895a216fc7054e57e4f20b114f06107d4
Chelle G.C <108133748+chellinha123@users.noreply.github.com>
2023-03-02 09:55:25 -0300

    Add files via upload

---
+++ Primeiro Projeto/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php echo "<h1>Ola mundo</h1>"?>
+ <h2> 8 +8 = <?php  echo(8+8)?></h2>
+ <h2> 8 - 8 = <?php  echo(8-8)?></h2>
+ <h2> 8 * 8 = <?php  echo(8*8)?></h2>
+ <h2> 8 / 2 = <?php  echo(8/2)?></h2>
